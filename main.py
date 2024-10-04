401d147e1d00bc3bde6ed7af3dd2dfca6d5f523e
mosiburpaik <67229063+mosiburpaik@users.noreply.github.com>
2024-10-04 18:50:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -241,14 +241,9 @@ class ByBit:
         return True
                 
     def add_query(self):
-        secret_code = animated_input("[yellow]Enter the secret code: [/yellow]")
-        if secret_code == "HACK":
-            query = animated_input("Enter the query to add: ")
             with open('data.txt', 'a') as f:
                 f.write(query + '\n')
             self.log("Query added successfully!", "SUCCESS")
-        else:
-            self.log("Invalid secret code. Access denied.", "ERROR")
 
     def add_proxy(self):
         proxy = animated_input("Enter the proxy to add: ")
